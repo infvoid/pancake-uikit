2890d07f0d855027891da1a4ab5a2968a085096e
infvoid <infvoid.defi@yandex.ru>
2021-06-06 16:51:29 +0800

    fix color

--- src/theme/colors.ts
+++ src/theme/colors.ts
@@ -28,7 +28,7 @@ export const lightColors: Colors = {
   tertiary: "#EFF4F5",
   text: "#452A7A",
   textDisabled: "#BDC2C4",
-  textSubtle: "#8f80ba",
+  textSubtle: "#303133",
   borderColor: "#E9EAEB",
   gradients: {
     bubblegum: "linear-gradient(139.73deg, #E6FDFF 0%, #F3EFFF 100%)",
