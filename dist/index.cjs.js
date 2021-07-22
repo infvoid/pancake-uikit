3f2db5c3fd3d6be3a159664cb577cb1be75d0e9d
Your Name <xxxhxh622@163.com>
2021-07-22 11:20:14 +0800

    ihos

--- dist/index.cjs.js
+++ dist/index.cjs.js
@@ -1969,22 +1969,23 @@ var getStepNumberFontColor = function (_a) {
     }
     return theme.colors.textDisabled;
 };
-var StyledStep = styled__default['default'](Flex)(templateObject_1$o || (templateObject_1$o = __makeTemplateObject(["\n  ", " {\n    justify-content: center;\n  }\n"], ["\n  ", " {\n    justify-content: center;\n  }\n"])), function (_a) {
+var StyledStep = styled__default['default'](Flex)(templateObject_1$o || (templateObject_1$o = __makeTemplateObject(["\n  ", " {\n    justify-content: left;\n  }\n"], ["\n  ", " {\n    justify-content: left;\n  }\n"])), function (_a) {
     var theme = _a.theme;
     return theme.mediaQueries.md;
 });
-var Connector = styled__default['default'].div(templateObject_2$b || (templateObject_2$b = __makeTemplateObject(["\n  position: absolute;\n  width: 4px;\n  height: 100%;\n  top: 50%;\n  left: calc(50% - 2px);\n  background-color: ", ";\n"], ["\n  position: absolute;\n  width: 4px;\n  height: 100%;\n  top: 50%;\n  left: calc(50% - 2px);\n  background-color: ", ";\n"])), function (_a) {
+var Connector = styled__default['default'].div(templateObject_2$b || (templateObject_2$b = __makeTemplateObject(["\n  position: absolute;\n  width: 4px;\n  height: 100%;\n  top: 50%;\n  left: calc(50% - 2px);\n  background-color: ", ";\n"], ["\n  position: absolute;\n  width: 4px;\n  height: 100%;\n  top: 50%;\n  left: calc(50% - 2px);\n  background-color: ",
+    ";\n"])), function (_a) {
     var theme = _a.theme, status = _a.status;
     return theme.colors[status === "past" ? "success" : "textDisabled"];
 });
-var ChildrenWrapper = styled__default['default'](Box)(templateObject_3$5 || (templateObject_3$5 = __makeTemplateObject(["\n  ", " {\n    visibility: ", ";\n  }\n"], ["\n  ", " {\n    visibility: ", ";\n  }\n"])), function (_a) {
+var ChildrenWrapper = styled__default['default'](Box)(templateObject_3$5 || (templateObject_3$5 = __makeTemplateObject(["\n  /* ", " {\n    visibility: ", ";\n  } */\n"], ["\n  /* ", " {\n    visibility: ", ";\n  } */\n"])), function (_a) {
     var theme = _a.theme;
     return theme.mediaQueries.md;
 }, function (_a) {
     var isVisible = _a.isVisible;
     return (isVisible ? "visible" : "hidden");
 });
-var ChildrenLeftWrapper = styled__default['default'](ChildrenWrapper)(templateObject_4$3 || (templateObject_4$3 = __makeTemplateObject(["\n  display: none;\n  ", " {\n    display: block;\n    margin-right: 16px;\n  }\n"], ["\n  display: none;\n  ", " {\n    display: block;\n    margin-right: 16px;\n  }\n"])), function (_a) {
+styled__default['default'](ChildrenWrapper)(templateObject_4$3 || (templateObject_4$3 = __makeTemplateObject(["\n  display: none;\n  ", " {\n    display: block;\n    margin-right: 16px;\n  }\n"], ["\n  display: none;\n  ", " {\n    display: block;\n    margin-right: 16px;\n  }\n"])), function (_a) {
     var theme = _a.theme;
     return theme.mediaQueries.md;
 });
@@ -1993,12 +1994,14 @@ var ChildrenRightWrapper = styled__default['default'](ChildrenWrapper)(templateO
     return theme.mediaQueries.md;
 });
 var Wrapper$2 = styled__default['default'].div(templateObject_6 || (templateObject_6 = __makeTemplateObject(["\n  position: relative;\n  display: flex;\n  align-items: center;\n"], ["\n  position: relative;\n  display: flex;\n  align-items: center;\n"])));
-var StepNumber = styled__default['default'].div(templateObject_7 || (templateObject_7 = __makeTemplateObject(["\n  box-shadow: 0px 1px 4px rgba(25, 19, 38, 0.15);\n  background-color: ", ";\n  border: 2px solid ", ";\n  border-radius: ", ";\n  color: ", ";\n  display: flex;\n  justify-content: center;\n  align-items: center;\n  font-weight: 600;\n  font-size: 32px;\n  width: 48px;\n  height: 48px;\n  z-index: 1;\n  ", " {\n    font-size: 40px;\n    width: 80px;\n    height: 80px;\n  }\n"], ["\n  box-shadow: 0px 1px 4px rgba(25, 19, 38, 0.15);\n  background-color: ", ";\n  border: 2px solid ", ";\n  border-radius: ", ";\n  color: ", ";\n  display: flex;\n  justify-content: center;\n  align-items: center;\n  font-weight: 600;\n  font-size: 32px;\n  width: 48px;\n  height: 48px;\n  z-index: 1;\n  ", " {\n    font-size: 40px;\n    width: 80px;\n    height: 80px;\n  }\n"])), function (_a) {
+var StepNumber = styled__default['default'].div(templateObject_7 || (templateObject_7 = __makeTemplateObject(["\n  box-shadow: 0px 1px 4px rgba(25, 19, 38, 0.15);\n  background-color: ", ";\n  border: 2px solid\n    ", ";\n  border-radius: ", ";\n  color: ", ";\n  display: flex;\n  justify-content: center;\n  align-items: center;\n  font-weight: 600;\n  font-size: 32px;\n  width: 48px;\n  height: 48px;\n  z-index: 1;\n  ", " {\n    font-size: 40px;\n    width: 80px;\n    height: 80px;\n  }\n"], ["\n  box-shadow: 0px 1px 4px rgba(25, 19, 38, 0.15);\n  background-color: ",
+    ";\n  border: 2px solid\n    ",
+    ";\n  border-radius: ", ";\n  color: ", ";\n  display: flex;\n  justify-content: center;\n  align-items: center;\n  font-weight: 600;\n  font-size: 32px;\n  width: 48px;\n  height: 48px;\n  z-index: 1;\n  ", " {\n    font-size: 40px;\n    width: 80px;\n    height: 80px;\n  }\n"])), function (_a) {
     var theme = _a.theme, status = _a.status;
     return theme.colors[status === "current" ? "secondary" : "invertedContrast"];
 }, function (_a) {
     var theme = _a.theme, status = _a.status;
-    return (status === "past" ? theme.colors.success : "transparent");
+    return status === "past" ? theme.colors.success : "transparent";
 }, function (_a) {
     var theme = _a.theme;
     return theme.radii.circle;
@@ -2012,13 +2015,11 @@ var StepNumber = styled__default['default'].div(templateObject_7 || (templateObj
  */
 var Step = function (_a) {
     var index = _a.index, status = _a.status, _b = _a.numberOfSteps, numberOfSteps = _b === void 0 ? 0 : _b, children = _a.children;
-    var isIndexPair = index % 2 === 0;
     return (React__default['default'].createElement(StyledStep, { mb: index < numberOfSteps - 1 ? "16px" : 0 },
-        React__default['default'].createElement(ChildrenLeftWrapper, { isVisible: !isIndexPair }, children),
         React__default['default'].createElement(Wrapper$2, null,
             React__default['default'].createElement(StepNumber, { status: status }, index + 1),
             index < numberOfSteps - 1 && React__default['default'].createElement(Connector, { status: status })),
-        React__default['default'].createElement(ChildrenRightWrapper, { isVisible: isIndexPair }, children)));
+        React__default['default'].createElement(ChildrenRightWrapper, null, children)));
 };
 var templateObject_1$o, templateObject_2$b, templateObject_3$5, templateObject_4$3, templateObject_5$1, templateObject_6, templateObject_7;
 
--- src/components/Stepper/Step.tsx
+++ src/components/Stepper/Step.tsx
@@ -16,7 +16,7 @@ const getStepNumberFontColor = ({ theme, status }: StatusProps) => {
 
 const StyledStep = styled(Flex)`
   ${({ theme }) => theme.mediaQueries.md} {
-    justify-content: center;
+    justify-content: left;
   }
 `;
 
@@ -26,13 +26,14 @@ const Connector = styled.div<StatusProps>`
   height: 100%;
   top: 50%;
   left: calc(50% - 2px);
-  background-color: ${({ theme, status }) => theme.colors[status === "past" ? "success" : "textDisabled"]};
+  background-color: ${({ theme, status }) =>
+    theme.colors[status === "past" ? "success" : "textDisabled"]};
 `;
 
-const ChildrenWrapper = styled(Box)<{ isVisible: boolean }>`
-  ${({ theme }) => theme.mediaQueries.md} {
+const ChildrenWrapper = styled(Box)<{ isVisible?: boolean }>`
+  /* ${({ theme }) => theme.mediaQueries.md} {
     visibility: ${({ isVisible }) => (isVisible ? "visible" : "hidden")};
-  }
+  } */
 `;
 
 const ChildrenLeftWrapper = styled(ChildrenWrapper)`
@@ -58,8 +59,11 @@ const Wrapper = styled.div`
 
 export const StepNumber = styled.div<StatusProps>`
   box-shadow: 0px 1px 4px rgba(25, 19, 38, 0.15);
-  background-color: ${({ theme, status }) => theme.colors[status === "current" ? "secondary" : "invertedContrast"]};
-  border: 2px solid ${({ theme, status }) => (status === "past" ? theme.colors.success : "transparent")};
+  background-color: ${({ theme, status }) =>
+    theme.colors[status === "current" ? "secondary" : "invertedContrast"]};
+  border: 2px solid
+    ${({ theme, status }) =>
+      status === "past" ? theme.colors.success : "transparent"};
   border-radius: ${({ theme }) => theme.radii.circle};
   color: ${getStepNumberFontColor};
   display: flex;
@@ -81,16 +85,22 @@ export const StepNumber = styled.div<StatusProps>`
  * ChildrenLeftWrapper and ChildrenRightWrapper are used on the non mobile version, to force the alternate layout.
  * One of the child is hidden based on the step number.
  */
-export const Step: React.FC<StepProps> = ({ index, status, numberOfSteps = 0, children }) => {
+export const Step: React.FC<StepProps> = ({
+  index,
+  status,
+  numberOfSteps = 0,
+  children,
+}) => {
   const isIndexPair = index % 2 === 0;
   return (
     <StyledStep mb={index < numberOfSteps - 1 ? "16px" : 0}>
-      <ChildrenLeftWrapper isVisible={!isIndexPair}>{children}</ChildrenLeftWrapper>
+      {/* <ChildrenLeftWrapper isVisible={!isIndexPair}>{children}</ChildrenLeftWrapper> */}
       <Wrapper>
         <StepNumber status={status}>{index + 1}</StepNumber>
         {index < numberOfSteps - 1 && <Connector status={status} />}
       </Wrapper>
-      <ChildrenRightWrapper isVisible={isIndexPair}>{children}</ChildrenRightWrapper>
+      {/* <ChildrenRightWrapper isVisible={isIndexPair}>{children}</ChildrenRightWrapper> */}
+      <ChildrenRightWrapper>{children}</ChildrenRightWrapper>
     </StyledStep>
   );
 };
